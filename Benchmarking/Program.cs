5bea1bb506c6cc80372b92a0dd0ac0b857ae6db1
Vincent Wong <vincentwwy168@gmail.com>
2025-06-24 12:49:44 +0800

    Prepare benchmarking project

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DictionaryListBenchmarking;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Benchmarking the DictionaryList.");
+
+        // var summary = BenchmarkRunner.Run<BenchmarkTest>();
+    }
+}
